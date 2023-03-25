1bcb34ecd2f7f4891e86226aff291ffd83f3f426
Lluvia <friasllu@gmail.com>
2023-03-24 20:41:36 -0400

    commit tree f4b8b837abc1ff0a12ec14468585880acb3624c4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each.with_index(1) { |name, i| puts "#{i}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect! { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(isit_cheese)
+  cheeses = %w[gouda cheddar camembert]
+
+  isit_cheese.find { |maybe_cheese|
+    cheeses.include?(maybe_cheese) }
+  
 end
